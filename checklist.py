dde2350976320a624d4fe50f30f904d383456686
Jackson Chadfield <contact@jacksonc.dev>
2026-02-25 20:52:50 +1300

    refactor docstrings

--- checklist.py
+++ checklist.py
@@ -26,6 +26,9 @@ FOOTER_TOP = 30
 TABLE_SEPARATOR_ODD = 220
 TABLE_SEPARATOR_EVEN = 193
 
+# If table parsing accuracy is below this threshold a ParseError will be raised.
+MIN_TABLE_PARSE_ACCURACY = 95
+
 class ParseError(Exception):
 	pass
 
@@ -42,20 +45,27 @@ class Checklist:
 			return Checklist(pdf_path)
 
 	def __init__(self, pdf_path: pathlib.Path):
-		self.path = pdf_path
-		self.width = self._get_pdf_width()
+		self._path = pdf_path
+		self._width = self._get_pdf_width()
 		pages = self._get_combined_pages()
-		tables = self._parse_tables(pages)
+		tables = self._extract_tables(pages)
 
 	def _get_pdf_width(self) -> int:
-		reader = PdfReader(self.path)
-		width = reader.pages[0].mediabox.width
+		"""Returns the width of the PDF pages, and checks that all pages have the same width."""
+		reader = PdfReader(self._path)
+		width = reader.pages[0].mediabox._width
 		for page in reader.pages[1:]:
-			if page.mediabox.width != width:
+			if page.mediabox._width != width:
 				raise ParseError("PDF pages have different widths, which is not supported")
 		return width
 
 	def _get_table_areas(self, odd: bool, first = False) -> list[str]:
+		"""
+		Returns the areas of a page where tables are located
+
+		Each area is defined as a string in the format "x1,y1,x2,y2", where (x1, y1) is the top-left corner and (x2, y2) is the bottom-right corner of the area.
+		The areas are different for odd and even pages, and the first page has a different header size
+		"""
 		if first and not odd:
 			raise ParseError("First page is odd")
 
@@ -63,59 +73,55 @@ class Checklist:
 		separator = TABLE_SEPARATOR_ODD if odd else TABLE_SEPARATOR_EVEN
 		return [
 			f"0,{top},{separator},{FOOTER_TOP}",
-			f"{separator},{top},{self.width},{FOOTER_TOP}"
+			f"{separator},{top},{self._width},{FOOTER_TOP}"
 		]
 
-	def _parse_tables(self, pages: list[int]) -> list[Table]:
+	def _extract_tables(self, pages: list[int]) -> list[Table]:
+		"""Extracts tables from the specified pages in the checklist PDF."""
 		tables: list[Table] = []
 
 		pages = sorted(pages)
 		if pages[0] == 1:
-			first_page = self._parse_tables_with_areas([pages.pop(0)], self._get_table_areas(odd=True, first=True))
+			first_page = self._extract_table_from_area([pages.pop(0)], self._get_table_areas(odd=True, first=True))
 			tables.extend(first_page)
 
 		odd_page_numbers = [p for p in pages if p % 2 == 1]
-		odd_pages = self._parse_tables_with_areas(odd_page_numbers, self._get_table_areas(odd=True))
+		odd_pages = self._extract_table_from_area(odd_page_numbers, self._get_table_areas(odd=True))
 		tables.extend(odd_pages)
 
 		even_page_numbers = [p for p in pages if p % 2 == 0]
-		even_pages = self._parse_tables_with_areas(even_page_numbers, self._get_table_areas(odd=False))
+		even_pages = self._extract_table_from_area(even_page_numbers, self._get_table_areas(odd=False))
 		tables.extend(even_pages)
 
-		print(f"Parsed {len(tables)} tables from the PDF with accuracy: {[t.parsing_report['accuracy'] for t in tables]}")
+		return tables
 
-		self._debug_camelot(tables[0])
-
-	def _parse_tables_with_areas(self, pages: list[int], areas: list[str]) -> TableList:
+	def _extract_table_from_area(self, pages: list[int], areas: list[str]) -> TableList:
+		"""Extracts tables from the specified pages and areas using camelot."""
 		pages_str = ",".join(str(p) for p in pages)
-		tables = camelot.read_pdf(self.path, pages=pages_str, flavor='network', table_areas=areas, parallel=True)
+		tables = camelot.read_pdf(self._path, pages=pages_str, flavor='network', table_areas=areas, parallel=True)
 
 		if tables.n != len(pages) * len(areas):
 			raise ParseError(f"Expected to find {len(pages) * len(areas)} tables, but found {tables.n}")
 
 		for table in tables:
-			if table.parsing_report['accuracy'] < 90:
+			if table.parsing_report['accuracy'] < MIN_TABLE_PARSE_ACCURACY:
 				raise ParseError(f"Low parsing accuracy: {table.parsing_report['accuracy']}%")
 
 		return tables
 
-
-	def _debug_camelot(self, table: Table):
-		camelot.plot(table).show()
-
-	"""
-	Finds the page numbers of the combined table in the checklist PDF.
-
-	The checklist PDF contains 3 sections:
-	* Combined table (All Volumes)
-	* Volume 2 & 3
-	* Volume 4
-
-	We only care about the combined table, which contains all the files that need to be updated.
-	The other tables are just subsets of the combined table, so we can ignore them.
-	"""
 	def _get_combined_pages(self) -> list[int]:
-		reader = PdfReader(self.path)
+		"""
+		Finds the page numbers of the combined table in the checklist PDF.
+
+		The checklist PDF contains 3 sections:
+		* Combined table (All Volumes)
+		* Volume 2 & 3
+		* Volume 4
+
+		We only care about the combined table, which contains all the files that need to be updated.
+		The other tables are just subsets of the combined table, so we can ignore them.
+		"""
+		reader = PdfReader(self._path)
 		result: list[int] = []
 
 		for page_num, page in enumerate(reader.pages, start=1):
@@ -140,6 +146,8 @@ class Checklist:
 				return result
 
 			result.append(page_num)
+
+		return result
 
 
 if __name__ == '__main__':
