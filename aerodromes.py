6e0e508f526d1c0e7ba45cd70f6376a1380eab43
Jackson Chadfield <contact@jacksonc.dev>
2026-03-01 16:53:12 +1300

    add aerodrome static_map

--- aerodromes.py
+++ aerodromes.py
@@ -1,3 +1,96 @@
-AERODROME_LOOKUP = {
-	'NZLX': 'Alexandra',
-}
+# Aerodromes
+AERODROME_STATIC_MAP = {'NZLX': 'ALEXANDRA', 'NZAN': 'ANAMA', 'NZAR': 'ARDMORE', 'NZAS': 'ASHBURTON',
+						'NZAM': 'ASHBURTON MEDICAL CENTRE', 'NZAA': 'AUCKLAND', 'NZAW': 'AUCKLAND HARBOUR',
+						'NZJL': 'AUCKLAND HOSPITAL', 'NZBA': 'BALCLUTHA', 'NZJI': 'BAY OF ISLANDS HOSPITAL',
+						'NZBW': 'BURWOOD HOSPITAL', 'NZCG': 'CENTENNIAL PARK', 'NZCB': 'CENTRE BUSH',
+						'NZCI': 'CHATHAM IS/TUUTA', 'NZCV': 'CHEVIOT', 'NZCH': 'CHRISTCHURCH',
+						'NZJC': 'CHRISTCHURCH HOSPITAL', 'NZJJ': 'CHRISTCHURCH HOSPITAL HAGLEY', 'NZCL': 'CLOUDY BAY',
+						'NZCS': 'CROMWELL RACECOURSE', 'NZDV': 'DANNEVIRKE', 'NZDA': 'DARGAVILLE',
+						'NZJE': 'DARGAVILLE HOSPITAL', 'NZDY': 'DRURY', 'NZDN': 'DUNEDIN', 'NZDC': 'DUNEDIN CITY',
+						'NZDH': 'DUNEDIN HOSPITAL', 'NZDU': 'DUNSTAN HOSPITAL', 'NZEV': 'EVES VALLEY',
+						'NZFI': 'FEILDING', 'NZFG': 'FERN GULLY', 'NZFE': 'FERNSIDE FIELDS', 'NZFT': 'FLAT POINT',
+						'NZFF': 'FOREST FIELD', 'NZFH': 'FOX', 'NZFO': 'FOX GLACIER', 'NZFP': 'FOXPINE',
+						'NZFR': 'FOX RIVER', 'NZFJ': 'FRANZ JOSEF', 'NZGS': 'GISBORNE', 'NZJG': 'GISBORNE HOSPITAL',
+						'NZGH': 'GLACIER COUNTRY', 'NZGY': 'GLENORCHY', 'NZGT': 'GLENTANNER', 'NZGC': 'GORE',
+						'NZGO': 'GORE HOSPITAL', 'NZGB': 'GREAT BARRIER', 'NZGM': 'GREYMOUTH', 'NZHT': 'HAAST',
+						'NZHN': 'HAMILTON', 'NZHM': 'HANMER', 'NZHR': 'HANMER MEDICAL CENTRE', 'NZHS': 'HASTINGS',
+						'NZJH': 'HASTINGS HOSPITAL', 'NZHA': 'HAWERA', 'NZHB': 'HELENA BAY', 'NZHK': 'HOKITIKA',
+						'NZHF': 'HUKA FALLS', 'NZNV': 'INVERCARGILL', 'NZKO': 'KAIKOHE',
+						'NZKC': 'KAIKOURA MEDICAL CENTRE', 'NZKF': 'KAIPARA FLATS', 'NZKT': 'KAITAIA',
+						'NZJK': 'KAITAIA HOSPITAL', 'NZKM': 'KARAMEA', 'NZWE': 'KAUAROA BAY',
+						'NZKH': 'KENEPURU HOSPITAL', 'NZKN': 'KENSINGTON PARK', 'NZKK': 'KERIKERI/BAY OF ISLANDS',
+						'NZKP': 'KOPUTAROA', 'NZKY': 'KOWHAI', 'NZKU': 'KUPE', 'NZHP': 'LAKE HAUPIRI',
+						'NZLE': 'LAKE STATION/NELSON LAKES', 'NZLA': 'LOBURN ABBEY', 'NZLM': 'LUMSDEN MEDICAL CENTRE',
+						'NZMW': 'MAKARORA', 'NZOG': 'MAKARORA HELIPORT', 'NZVL': 'MANDEVILLE', 'NZMG': 'MANGONUI',
+						'NZSO': 'MARLBOROUGH SOUNDS', 'NZMA': 'MATAMATA', 'NZUA': 'MAUI A', 'NZUB': 'MAUI B',
+						'NZMB': 'MECHANICS BAY', 'NZME': 'MERCER', 'NZMV': 'METHVEN', 'NZMM': 'MIDDLEMORE HOSPITAL',
+						'NZML': 'MID WAIHO LOOP', 'NZMF': 'MILFORD SOUND', 'NZMK': 'MOTUEKA',
+						'NZKD': 'MOTU KAIKOURA ISLAND', 'NZMC': 'MOUNT COOK', 'NZMR': 'MURCHISON',
+						'NZUR': 'MURCHISON HOSPITAL', 'NZNR': 'NAPIER', 'NZNS': 'NELSON', 'NZNH': 'NELSON HOSPITAL',
+						'NZNP': 'NEW PLYMOUTH', 'NZNF': 'NORFOLK', 'NZNE': 'NORTH SHORE',
+						'NZJN': 'NORTH SHORE HOSPITAL', 'NZON': 'OBAN', 'NZOB': 'OCEAN BEACH', 'NZOH': 'OHAKEA',
+						'NZOX': 'OKIWI STATION', 'NZOF': 'OMAHA FLATS', 'NZOM': 'OMAKA', 'NZOA': 'OMARAMA',
+						'NZOW': 'ONETANGI', 'NZOI': 'ONGAIO ISLAND', 'NZOP': 'OPOTIKI', 'NZOT': 'OTAKI',
+						'NZOS': 'OTEHEI BAY', 'NZPA': 'PAIHIA', 'NZPS': 'PAIHIA WATERFRONT', 'NZPM': 'PALMERSTON NORTH',
+						'NZJM': 'PALMERSTON NORTH HOSPITAL', 'NZPW': 'PAPAWAI', 'NZPI': 'PARAKAI', 'NZPN': 'PICTON',
+						'NZPK': 'PIKES POINT', 'NZPO': 'PORANGAHAU', 'NZPH': 'PUDDING HILL', 'NZUK': 'PUKAKI',
+						'NZPU': 'PUKEKOHE', 'NZQN': 'QUEENSTOWN', 'NZQW': 'QUEENS WHARF', 'NZRA': 'RAGLAN',
+						'NZRI': 'RAKITATA ISLAND', 'NZRN': 'RANFURLY MEDICAL CENTRE', 'NZRT': 'RANGIORA',
+						'NZRK': 'RANGITAIKI', 'NZMX': 'RAROA', 'NZJW': 'RAWENE HOSPITAL', 'NZRD': 'ROSEDALE',
+						'NZRO': 'ROTORUA', 'NZJO': 'ROTORUA HOSPITAL', 'NZLF': 'ROTORUA LAKEFRONT',
+						'NZRL': 'ROTORUA LAKES', 'NZRX': 'ROXBURGH', 'NZRC': 'RYANS CREEK',
+						'NZJS': 'SOUTHLAND-KEW HOSPITAL', 'NZSF': 'SPRINGFIELD', 'NZSL': 'SPRINGHILL',
+						'NZSD': 'STRATFORD', 'NZAH': 'TAHAROA IRONSANDS', 'NZTI': 'TAIERI', 'NZVR': 'TAIHAPE',
+						'NZTK': 'TAKAKA', 'NZTC': 'TAPANUI MEDICAL CENTRE', 'NZJQ': 'TARANAKI BASE HOSPITAL',
+						'NZTS': 'TASMAN', 'NZTM': 'TAUMARUNUI', 'NZJT': 'TAUMARUNUI HOSPITAL', 'NZAP': 'TAUPO',
+						'NZJZ': 'TAUPO HOSPITAL', 'NZLT': 'TAUPO WATER', 'NZTG': 'TAURANGA',
+						'NZJA': 'TAURANGA HOSPITAL', 'NZTJ': 'TAWHAKI', 'NZMO': 'TE ANAU / MANAPOURI',
+						'NZFA': 'TE HAPUA', 'NZTL': 'TEKAPO', 'NZTP': 'TEKAPO / MACKENZIE', 'NZTE': 'TE KOWHAI',
+						'NZTT': 'TE KUITI', 'NZTQ': 'TE KUITI HOSPITAL', 'NZJP': 'TE PUIA SPRINGS HOSPITAL',
+						'NZTH': 'THAMES', 'NZJD': 'THAMES HOSPITAL', 'NZTU': 'TIMARU', 'NZTZ': 'TIMARU HOSPITAL',
+						'NZTO': 'TOKOROA', 'NZJX': 'TOKOROA HOSPITAL', 'NZTN': 'TURANGI',
+						'NZTW': 'TWIZEL MEDICAL CENTRE', 'NZKE': 'WAIHEKE', 'NZWV': 'WAIHI BEACH',
+						'NZHH': 'WAIKATO HOSPITAL', 'NZWM': 'WAIMATE', 'NZYP': 'WAIPUKURAU',
+						'NZMH': 'WAIRARAPA HOSPITAL', 'NZJY': 'WAIROA HOSPITAL', 'NZWQ': 'WAITIKI', 'NZWF': 'WANAKA',
+						'NZHC': 'WANAKA LAKES HEALTH CENTRE', 'NZWZ': 'WARKWORTH', 'NZWN': 'WELLINGTON',
+						'NZWH': 'WELLINGTON HOSPITAL', 'NZWJ': 'WELLSFORD', 'NZWL': 'WEST MELTON', 'NZWS': 'WESTPORT',
+						'NZWK': 'WHAKATANE', 'NZJF': 'WHAKATANE HOSPITAL', 'NZWG': 'WHANGAMATA', 'NZWU': 'WHANGANUI',
+						'NZJU': 'WHANGANUI HOSPITAL', 'NZWR': 'WHANGAREI', 'NZJR': 'WHANGAREI HOSPITAL',
+						'NZES': 'WHAREPAPA SOUTH', 'NZWP': 'WHENUAPAI'}
+
+
+async def _main():
+	"""
+	Quick and dirty script to extract aerodrome ICAO mappings from the AIP aerodrome coordinates PDF.
+	"""
+	import camelot
+
+	from download import RichProgressReporter, downloader, DownloadJob
+
+	aerodrome_table_job = DownloadJob(
+		url='https://www.aip.net.nz/assets/AIP/Air-Navigation-Register/5-Aerodromes/NZANR-Aerodrome_Coordinates.pdf',
+		content_types=['application/pdf'])
+	with RichProgressReporter() as progress:
+		aerodrome_path = await downloader.download(aerodrome_table_job, progress=progress)
+
+	aerodromes: dict[str, str] = {}
+
+	tables = camelot.read_pdf(str(aerodrome_path), pages='all', parallel=True)
+	for table in tables:
+		# skip the first 2 rows which are just headers
+		for _, row in table.df.iloc[2:].iterrows():
+			name = row[0].strip()
+			icao = row[2].strip()
+			if not icao or not name:
+				raise ValueError(f"Invalid row in aerodrome table: {row}")
+			if not icao.startswith('NZ'):
+				raise ValueError(f"Invalid ICAO code in aerodrome table: {icao}")
+			aerodromes[icao] = name
+
+	print(aerodromes)
+
+
+if __name__ == '__main__':
+	import asyncio
+
+	raise SystemExit(asyncio.run(_main()))
