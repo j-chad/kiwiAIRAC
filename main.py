3f3fa554d9b1e8ad0383a1c6c3981f0074a23606
Jackson Chadfield <contact@jacksonc.dev>
2026-03-02 21:05:46 +1300

    add main.py for testing interface

---
+++ main.py
@@ -0,0 +1,18 @@
+from datetime import date
+
+from checklist import Checklist
+from models import Subscription
+
+
+async def main():
+	checklist = await Checklist.fetch()
+	checklist.volumes(Subscription.VISUAL).effective_after(date(2026, 2, 17))
+	checklist.for_duplex_printing()
+
+	update_pdf = stitch_duplex(checklist)
+	change_pdf = generate_change_pdf(checklist)
+
+
+if __name__ == "__main__":
+	import asyncio
+	raise SystemExit(asyncio.run(main()))
